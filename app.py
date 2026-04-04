d6d834c4319521f38d19ffc33d0ff15b27223c53
amira12hjk56-eng <amira12hjk56@gmail.com>
2026-04-04 17:24:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ model = joblib.load('diabetes_model.pkl')
 
 st.title("نظام التنبؤ بالسكري")
 age = st.number_input("العمر", min_value=1)
+
 if st.button("النتيجة"):
-prediction = model.predict([[age, 0, 0, 0, 0, 0, 0, 0]])
-    st.write(f"النتيجة: {prediction[0]}")
+    # هنا لازم السطرين دول يكونوا داخلين لجوه (مسافة Tab أو 4 مسافات)
+    prediction = model.predict([[age, 0, 0, 0, 0, 0, 0, 0]])
+    st.write(f"النتيجة: {prediction}")
    
    
